942707015c2df988932d6c8bf02c889b25cdead5
RareHat <makariakmaksum@gmail.com>
2024-12-10 18:25:06 +0200

    1212
    1212

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pygame
 import sys
 
+import self
+
 pygame.init()
 window = pygame.display.set_mode((0,0))
 
@@ -13,6 +15,7 @@ class Rocket:
         self.hitbox.x = x
         self.hitbox.y = y
         self.speed = speed
+        self.bullets = []
     def move(self):
         keys = pygame.key.get_pressed()
         if keys [pygame.K_RIGHT]:
@@ -23,10 +26,19 @@ class Rocket:
             self.hitbox.y -= self.speed
         if keys [pygame.K_LEFT]:
             self.hitbox.x -= self.speed
+        if keys[pygame.K_x]:
+            self.bullets.append(Bullet(10,
+                                       10, 10,
+                                       self.hitbox.x, self.hitbox.y,
+                                       "bullet.png"))
 
+        for bullet in self.bullets:
+            bullet.move()
 
     def draw(self, window):
         window.blit(self.texture, self.hitbox)
+        for bullet in self.bullets:
+            bullet.draw(window)
 class UFO:
     def __init__(self,speed, width , height, x, y, skin):
         self.texture = pygame.image.load(skin)
@@ -36,6 +48,43 @@ class UFO:
         self.hitbox.y = y
     def draw(self, window):
         window.blit(self.texture, self.hitbox)
+class Asteroid:
+    def _init__(self,speed,width,height,x ,y, skin):
+        self.texture = pygame.image.load(skin)
+        self.texture = pygame.transform.scale(self.texture, [width, height])
+        self.hitbox.x = x
+        self.hitbox.y = y
+    def draw(self, window):
+        window.blit(self.texture, self.hitbox)
+
+
+
+
+
+
+class Bullet:
+    def __init__(self, speed,   width, height, x , y, skin):
+        self.texture = pygame.image.load(skin)
+        self.texture = pygame.transform.scale(self.texture, [width, height])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+    def move(self):
+            self.hitbox.y -= self.speed
+
+    def draw(self, window):
+        window.blit(self.texture, self.hitbox)
+
+
+
+
+
+
+
+
+
 
 UFOs = [
      UFO(1, 100,100,100, 100,"ufo.png"),
@@ -47,25 +96,34 @@ UFOs = [
      UFO(1, 100,100,700, 100,"ufo.png"),
      UFO(1, 100,100,800, 100,"ufo.png"),
      UFO(1, 100,100,900, 100,"ufo.png"),
-     UFO(1, 100,100,1000, 200,"ufo.png"),
-     UFO(1, 100,100,1100, 200,"ufo.png"),
-     UFO(1, 100,100,1200, 200,"ufo.png"),
-     UFO(1, 100,100,1300, 200,"ufo.png"),
-     UFO(1, 100,100,1400, 200,"ufo.png"),
-     UFO(1, 100,100,1500, 200,"ufo.png"),
-     UFO(1, 100,100,1600, 200,"ufo.png"),
+     UFO(1, 100,100,1000, 100,"ufo.png"),
+     UFO(1, 100,100,1100, 100,"ufo.png"),
+     UFO(1, 100,100,100, 100,"ufo.png"),
+     UFO(1, 100,100,1100, 100,"ufo.png"),
+     UFO(1, 100,100,1200, 100,"ufo.png"),
+     UFO(1, 100,100,1300, 100,"ufo.png"),
 
-
-
-
-
-
-
-
+     UFO(1, 100, 100, 200, 200, "ufo.png"),
+     UFO(1, 100, 100, 300, 200, "ufo.png"),
+     UFO(1, 100, 100, 400, 200, "ufo.png"),
+     UFO(1, 100, 100, 500, 200, "ufo.png"),
+     UFO(1, 100, 100, 600, 200, "ufo.png"),
+     UFO(1, 100, 100, 700, 200, "ufo.png"),
+     UFO(1, 100, 100, 800, 200, "ufo.png"),
+     UFO(1, 100, 100, 900, 200, "ufo.png"),
+     UFO(1, 100, 100, 1000, 200, "ufo.png"),
+     UFO(1, 100, 100, 1100, 200, "ufo.png"),
+     UFO(1, 100, 100, 100, 200, "ufo.png"),
+     UFO(1, 100, 100, 1100, 200, "ufo.png"),
+     UFO(1, 100, 100, 1200, 200, "ufo.png"),
+     UFO(1, 100, 100, 1300, 200, "ufo.png"),
 ]
+#asteroids = [
+    #Asteroid( 3 , 120, 120, 100, 1200,  "asteroid.png"),
 
 
 
+#]
 
 
 
@@ -98,7 +156,8 @@ while True:
     player.draw(window)
     for ufo in UFOs:
         ufo.draw(window)
-
+    #for asteroid in Asteroids:
+        #asteroid.draw(window)
 
 
     pygame.display.flip()
